a9715906ac78d14cbbb339786ba5f7727e30b242
sillybear <1056668533@qq.com>
2017-11-11 01:42:37 +0800

    '写了音乐播放'

--- music_player/player.js
+++ music_player/player.js
@@ -1,16 +1,41 @@
 // 命名空间，模块化封装
 var MusicPlayer = {
     // 将页面打开时的高峰时间，少做事，做最核心的
+    playList: [],
     $bg: null,
-    init: function () {
-        // $由jquery找到元素DOM
+    $songName: null,
+    $artist: null,
+    $currentSong: null,
+    initData: function () {
         this.$bg = $('#bg');
         console.log(this.$bg);
-        this.$bg.css({
-            'background-image' : 'url(http://p3.music.126.net/nlleOhU4RFqoa7Rud3cCUg==/109951163050635616.jpg)'
-        })
+    },
+    updataSong: function () {
+        
+    },
+    init: function () {
+        // $由jquery找到元素DOM
+        this.initData();
+        // this.$bg = $('#bg');
+        this.updataSong();
+        // this.$bg.css({
+        //     'background-image' : 'url(http://p3.music.126.net/nlleOhU4RFqoa7Rud3cCUg==/109951163050635616.jpg)'
+        // })
     }
 }
 window.onload = function () {
-    MusicPlayer.init();
+    // ajax js 数据请求
+    var url = './resource/play_list.json';
+    $.ajax({ 
+        url: url,
+        type: 'GET',
+        dataType: 'json',
+        success: function (data) {
+            MusicPlayer.playList = data.result.tracks;
+            MusicPlayer.init();
+            // console.log(data.result.coverImgUrl);
+            // console.log(data.result.tracks);
+        }
+    })
+    
 }
