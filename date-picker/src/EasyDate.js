d36269bf4375a6bba2a0463e756016f66e7a68d1
YanTing1111 <1178170499@qq.com>
2017-12-29 16:21:00 +0800

    '项目包'

---
+++ date-picker/src/EasyDate.js
@@ -0,0 +1,10 @@
+class EasyDate{
+    constructor(offset){
+        this.base = new Date();
+        this.base.setHours(0);
+        this.base.setMinutes(0);
+        this.base.setSeconds(0);
+        this.base.setMilliseconds(0);
+    }
+    
+}
