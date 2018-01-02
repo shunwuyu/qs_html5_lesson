67f7f7b31e7005931318f01f926995f6c9fcf01e
Huzhangjie <1534634433@qq.com>
2018-01-02 22:00:50 +0800

    入口entry 出口output 加载模块module

---
+++ date-picker/webpack.config.js
@@ -0,0 +1,35 @@
+const webpack = require('webpack')
+const path= require('path')
+module.exports = {
+  // 入口
+  entry: {
+    // bundle 打包
+    'tqb-date-picker':'./src/main.js'
+  },
+  output: {
+    filename: '[name].bundle.js',
+    // dist windows linux
+    // C:\  /var/root/
+    // __dirname 得到一个绝对路径 node的常量
+    path: path.resolve(__dirname, './dist')
+  },
+  module: {
+    // 加载器 import .js 使用babel-loader 自动babel 编译
+    // stylus  stylus-loader
+    loaders: [
+      {
+        test: /\.js/,
+        exclude: /node_modules/,
+        loaders: ["babel-loader", "eslint-loader"]
+      }
+    ]
+  },
+  externals: {
+    'jquery': "jQuery"
+  },
+  watch: true,
+  watchOptions: {
+    ignored: /node_modules|dist|build|docs|css/,
+    poll: 1000
+  }
+}
