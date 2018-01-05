a69059bba407d7c0bd3b46e83eecff89955bba6d
caicong <1161403069@qq.com>
2018-01-05 21:40:21 +0800

    引用了handlebars模块

--- date-picker/app/DatePicker.js
+++ date-picker/app/DatePicker.js
@@ -1,5 +1,26 @@
+import $ from 'jquery';
+// webpack不会重复打包
+import template from '../template/picker.hbs';
+
 export default class DatePicker {
     constructor(el,options) {
         console.log('DatePicker');
+        this.createElement(options);
+        this.delegateEvent(options);
+    }
+    createElement(options) {
+        let o = {
+            static: true,
+            scattered: true,
+            confirm: true
+        }
+        let item = $(template(o));
+        item.appendTo(document.body);
+        setTimeout(() => {
+            item.removeClass('out');
+        },10)
+    }
+    delegateEvent(options) {
+
     }
 }
